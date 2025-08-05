ae5bf66805224af5e43688d5e002e834046c71b5
kiirodedi <moraismoralesdeborafernanda3@gmail.com>
2025-08-05 15:16:46 -0300

    criação da função de ddd

---
+++ services/ddd.js
@@ -0,0 +1,11 @@
+export const buscarDDDCallBack = async(ddd, callback) => {
+    let urlAPI = `https://brasilapi.com.br/api/ddd/v1/${ddd}`;
+    fetch(resposta => {
+        if(!resposta.ok){
+            throw new Error("Falha no fetch.");
+        }
+        return resposta.json();
+    })
+    .then(resposta => callback(resposta))
+    .catch(error => console.error("Erro: ", error));
+}
